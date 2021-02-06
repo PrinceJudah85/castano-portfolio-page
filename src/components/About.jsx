d9921e2aeb5bef0e4fbcbb9e18f096f445c55f8c
Rafael Castano <rafa328@gmail.com>
2021-02-06 05:22:46 -0500

    - Updated responsive css design for all mobile devices - Minor changes needed for completion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,8 +12,6 @@ export default function About() {
         <div className="about-right">
           <p>I am a goal-oriented software engineer with a deep interest in algorithmic problem solving and creative project building. My eagerness to learn combined with the ever expanding ceiling of tech, has allowed me to find deep fulfillment in this industry. My past experiences in working with the public has helped me to refine my communication and interpersonal skills which allow me to work cohesively within a group of any and all types of personalities.</p>
         </div>
-        <br />
-        <br />
       </div>
     </div>
   )
